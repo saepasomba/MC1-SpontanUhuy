717856affe4fe630078c21a852f52453901242b8
Sae Pasomba <saepasomba12@gmail.com>
2023-04-17 00:16:38 +0700

    Initial Commit

---
+++ MC1-SpontanUhuy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MC1-SpontanUhuy
+//
+//  Created by Sae Pasomba on 17/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
